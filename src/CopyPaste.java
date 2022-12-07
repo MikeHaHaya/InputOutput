063a38afd4a91d2be16a28a1effc3b6a5fe3cfea
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-12-07 13:49:09 +0200

    Copy-Paste

---
+++ src/CopyPaste.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class CopyPaste {
+
+    public static void main(String[] args) {
+
+        File inputFile = new File("C:\\Users\\danie\\Downloads\\Profile.pdf");
+        File outputFile = new File("C:\\Users\\danie\\Downloads\\Profile.pdf");
+
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+
+    }
+
+
+}
